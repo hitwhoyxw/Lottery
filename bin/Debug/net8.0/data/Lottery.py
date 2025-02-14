fd854f2fd288a80b968c327c5cf5ec272198c2fe
hitwwoyxw <1634216025@qq.com>
2025-02-14 17:45:27 +0800

    修改

--- bin/Debug/net8.0/data/Lottery.py
+++ bin/Debug/net8.0/data/Lottery.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
-"""
-Author: BigCat
-"""
+
 import argparse
 import os
 import requests
@@ -72,25 +70,112 @@ def spider(name, start, end):
             print("抱歉，没有找到数据源！")
 
     df = pd.DataFrame(data)
-    current_date = datetime.now().strftime("%Y%m%d")
-    df.to_csv("{}{}".format(name, current_date), encoding="utf-8")
+    df.to_csv("{}{}.csv".format(name, end), encoding="utf-8",index=False)
     return pd.DataFrame(data)
 
+
+def process_data(path,current_num):
+    df = pd.read_csv(path)
+    results=calculate_all_periods(df,current_num)
+        # 输出文件路径
+    output_file = 'ssq{}.csv'.format(current_num)
+    write_results_to_csv(df, results, output_file)
+    print(f"统计结果已写入文件: {output_file}")
+
+def calculate_statistics(df):
+    # 提取红球和蓝球数据
+    red_balls = df[['红球_1', '红球_2', '红球_3', '红球_4', '红球_5', '红球_6']]
+    blue_balls = df['蓝球']
+    
+    # 计算红球和蓝球的出现次数
+    red_counts = red_balls.apply(pd.Series.value_counts).sum(axis=1).sort_index()
+    blue_counts = blue_balls.value_counts().sort_index()
+    
+    # 计算红球和蓝球的出现频率（平均概率）
+    red_freq = red_counts / len(df)
+    blue_freq = blue_counts / len(df)
+    
+    return red_counts, red_freq, blue_counts, blue_freq
+
+# 计算不同时间段的统计数据
+def calculate_all_periods(df):
+    # 所有期
+    all_red_counts, all_red_freq, all_blue_counts, all_blue_freq = calculate_statistics(df)
+    
+    # 近100期
+    recent_100 = df.iloc[-100:]
+    recent_100_red_counts, recent_100_red_freq, recent_100_blue_counts, recent_100_blue_freq = calculate_statistics(recent_100)
+    
+    # 近50期
+    recent_50 = df.iloc[-50:]
+    recent_50_red_counts, recent_50_red_freq, recent_50_blue_counts, recent_50_blue_freq = calculate_statistics(recent_50)
+    
+    # 近30期
+    recent_30 = df.iloc[-30:]
+    recent_30_red_counts, recent_30_red_freq, recent_30_blue_counts, recent_30_blue_freq = calculate_statistics(recent_30)
+    
+    return {
+        'all_red_counts': all_red_counts,
+        'all_red_freq': all_red_freq,
+        'all_blue_counts': all_blue_counts,
+        'all_blue_freq': all_blue_freq,
+        'recent_100_red_counts': recent_100_red_counts,
+        'recent_100_red_freq': recent_100_red_freq,
+        'recent_100_blue_counts': recent_100_blue_counts,
+        'recent_100_blue_freq': recent_100_blue_freq,
+        'recent_50_red_counts': recent_50_red_counts,
+        'recent_50_red_freq': recent_50_red_freq,
+        'recent_50_blue_counts': recent_50_blue_counts,
+        'recent_50_blue_freq': recent_50_blue_freq,
+        'recent_30_red_counts': recent_30_red_counts,
+        'recent_30_red_freq': recent_30_red_freq,
+        'recent_30_blue_counts': recent_30_blue_counts,
+        'recent_30_blue_freq': recent_30_blue_freq,
+    }
+# 将结果写入CSV文件的新列中
+def write_results_to_csv(df, results, output_file):
+    # 创建一个新的DataFrame来存储结果
+    result_df = pd.DataFrame(index=range(1, 34))  # 红球范围是1-33，蓝球范围是1-16
+    
+    # 填充结果
+    result_df['all_red_counts'] = results['all_red_counts']
+    result_df['all_red_freq'] = results['all_red_freq']
+    result_df['recent_100_red_counts'] = results['recent_100_red_counts']
+    result_df['recent_100_red_freq'] = results['recent_100_red_freq']
+    result_df['recent_50_red_counts'] = results['recent_50_red_counts']
+    result_df['recent_50_red_freq'] = results['recent_50_red_freq']
+    result_df['recent_30_red_counts'] = results['recent_30_red_counts']
+    result_df['recent_30_red_freq'] = results['recent_30_red_freq']
+    
+    # 蓝球的范围是1-16
+    blue_result_df = pd.DataFrame(index=range(1, 17))
+    blue_result_df['all_blue_counts'] = results['all_blue_counts']
+    blue_result_df['all_blue_freq'] = results['all_blue_freq']
+    blue_result_df['recent_100_blue_counts'] = results['recent_100_blue_counts']
+    blue_result_df['recent_100_blue_freq'] = results['recent_100_blue_freq']
+    blue_result_df['recent_50_blue_counts'] = results['recent_50_blue_counts']
+    blue_result_df['recent_50_blue_freq'] = results['recent_50_blue_freq']
+    blue_result_df['recent_30_blue_counts'] = results['recent_30_blue_counts']
+    blue_result_df['recent_30_blue_freq'] = results['recent_30_blue_freq']
+    
+    # 将红球和蓝球的结果合并到一个CSV文件中
+    result_df.to_csv(output_file, index_label='号码')
+    blue_result_df.to_csv(output_file, mode='a', index_label='号码')  # 追加蓝球数据
+
 def run(name):
-    """
-    :param name: 玩法名称
-    :return:
-    """
     current_number = get_current_number(name)
     print("【{}】最新一期期号：{}".format(name,current_number))
     if not os.path.exists(fold):
         os.makedirs(fold)
-    data = spider(name, 1, current_number)
     dataFolder= os.path.join(os.getcwd(),fold)
-    current_date = datetime.now().strftime("%Y%m%d")
-    if "{}{}".format(name, current_date) in os.listdir(dataFolder):
-        print("【{}】数据准备就绪，共{}期, ...".format(current_number, len(data)))
 
+    if "{}{}".format(name, current_number) in os.listdir(dataFolder):
+        print("【{}】数据已经存在，共{}期, ...".format(current_number, len(data)))
+    data = spider(name, 1, current_number)
+    dataFile = "{}{}".format(name, current_number)
+    if dataFile in os.listdir(dataFolder):
+        print("【{}】数据准备就绪，共{}期, ...".format(current_number, len(data)))
+        process_data(dataFile,current_number)
 if __name__ == "__main__":
     if not args.name:
         raise Exception("玩法名称不能为空！")
